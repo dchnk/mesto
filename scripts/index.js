241d6268f725cb09e61bf881242ddb480ca64c1a
dchnk <seroxe2@yandex.ru>
2023-02-03 12:43:05 +0700

    feat: добавить создание и удаление карточек, возможность поставить и убрать лайк

--- scripts/index.js
+++ scripts/index.js
@@ -1,46 +1,164 @@
-// Open/Close POPUP
+// CardsArray
+
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+]; 
+
+// Создаем карточки из стандартного массива
+
+const cardsContainer = document.querySelector('.elements');
+
+function addCard(cardName, cardLink) {
+    const cardTemplate = document.querySelector('#content-element').content;
+    const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+  
+    cardElement.querySelector('.element__heading').textContent = cardName;
+    cardElement.querySelector('.element__photo').alt = cardName;
+    cardElement.querySelector('.element__photo').src = cardLink;
+    cardElement.querySelector('.element__bin').addEventListener('click', function (e) {
+        e.target.closest('.element').remove();
+    });
+    cardElement.querySelector('.element__like').addEventListener('click', function (e) {
+        e.target.classList.toggle('element__like_active');
+    })
+    cardsContainer.append(cardElement);
+}
+
+initialCards.map(function (item) {
+    addCard(item.name, item.link)
+});
+
+// POPUP OPEN/CLOSE
+
+document.querySelectorAll('.popup__close').forEach(function (item) {
+    const closestPopup = item.closest('.popup');
+    item.addEventListener('click', function() {
+    
+        closePopup(closestPopup);
+    })
+});
+
+function openPopup(item) {
+    item.classList.add('popup_opened');
+};
+
+function closePopup(item) {    
+    item.classList.remove('popup_opened');
+};
+
+// POPUP EDIT
 
 const profileEdit = document.querySelector('.profile__edit');
-const popup = document.querySelector('.popup');
-const popupClose = document.querySelector('.popup__close');
+const popupEdit = document.querySelector('.popup_type_edit');
 
-function openPopup() {
+profileEdit.addEventListener("click", function(e) {
+   if (e.target === profileEdit) {
     nameInput.value = profileHeading.textContent;
     jobInput.value = profileDescription.textContent;
-    popup.classList.add('popup_opened');
-};
-
-function closePopup() {    
-    popup.classList.remove('popup_opened');
-};
-
-profileEdit.addEventListener("click", openPopup);
-popupClose.addEventListener("click", closePopup);
-
+    openPopup(popupEdit);
+   }
+});
 
 // Take input value from profile
 
-let profileHeading = document.querySelector('.profile__heading');
-let profileDescription = document.querySelector('.profile__description');
-let nameInput = document.querySelector('.popup__input_type_name');
-let jobInput = document.querySelector('.popup__input_type_job');
+const profileHeading = document.querySelector('.profile__heading');
+const profileDescription = document.querySelector('.profile__description');
+const nameInput = document.querySelector('.popup__input_type_name');
+const jobInput = document.querySelector('.popup__input_type_job');
 
 
 
 // Send new values for profile from editForm
 
-let formElement = document.querySelector('.popup__form');
+let formElementEdit = document.querySelector('.popup__form_type_edit');
 
 // Обработчик «отправки» формы
-function handleFormSubmit (evt) {
-    evt.preventDefault();    
+function editFormSubmit (evt) {
+    evt.preventDefault();
     
     let currentInputName = nameInput.value;    
     let currentInputJob = jobInput.value;
     
     profileHeading.textContent = currentInputName;
     profileDescription.textContent = currentInputJob;
-    closePopup();
+    closePopup(popupEdit);
 }
 
-formElement.addEventListener('submit', handleFormSubmit);
+formElementEdit.addEventListener('submit', editFormSubmit);
+
+
+// POPUP CARD
+
+const popupCard = document.querySelector('.popup_type_card');
+const addCardButton = document.querySelector('.profile__add');
+
+addCardButton.addEventListener("click", function(e) {
+    if (e.target === addCardButton) {
+      photoNameInput.value = "";
+      linkInput.value = "";
+      openPopup(popupCard);
+    }
+});
+
+const photoNameInput = document.querySelector('.popup__input_type_photo-name');
+const linkInput = document.querySelector('.popup__input_type_link');
+
+let formElementCard = document.querySelector('.popup__form_type_card');
+
+function addFormSubmit (evt) {
+    evt.preventDefault();
+    
+    let currentInputPhotoName = photoNameInput.value;    
+    let currentInputLink = linkInput.value;
+    addNewCard(currentInputPhotoName, currentInputLink);
+    closePopup(popupCard);
+}
+
+function addNewCard(cardName, cardLink) {
+    const cardTemplate = document.querySelector('#content-element').content;
+    const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+  
+    cardElement.querySelector('.element__heading').textContent = cardName;
+    cardElement.querySelector('.element__photo').alt = cardName;
+    cardElement.querySelector('.element__photo').src = cardLink;
+    cardElement.querySelector('.element__bin').addEventListener('click', function (e) {
+        e.target.closest('.element').remove();
+    });
+    cardElement.querySelector('.element__like').addEventListener('click', function (e) {
+        e.target.classList.toggle('element__like_active');
+    });
+    cardsContainer.prepend(cardElement);
+}
+
+formElementCard.addEventListener('submit', addFormSubmit);
+
+
+
+
+
+
+
+
